fee844982ff95d1145afc1ba9f08e12ed7975301
Your name <your.email@example.com>
2026-07-29 16:37:29 +0530

    testing again  index.ts

--- src/repo/index.ts
+++ src/repo/index.ts
@@ -2,14 +2,28 @@ import type { Repository } from "@/repo/repository";
 import { createLocalRepo } from "@/repo/localRepo";
 import { createApiRepo } from "@/repo/apiRepo";
 
-console.log(import.meta.env.VITE_USE_API);
 let repo: Repository | null = null;
+console.log(import.meta.env.VITE_USE_API);
 
+// export function getRepo(): Repository {
+//   if (!repo) {
+//     if (import.meta.env.VITE_USE_API === "true") {
+//       repo = createApiRepo();
+//     } else {
+//       repo = createLocalRepo();
+//     }
+//   }
+//   return repo;
+// }
 export function getRepo(): Repository {
   if (!repo) {
+    console.log("VITE_USE_API =", import.meta.env.VITE_USE_API);
+
     if (import.meta.env.VITE_USE_API === "true") {
+      console.log("Using API Repo");
       repo = createApiRepo();
     } else {
+      console.log("Using Local Repo");
       repo = createLocalRepo();
     }
   }
